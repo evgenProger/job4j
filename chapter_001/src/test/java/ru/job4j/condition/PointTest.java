be3d0c64f7d3694fee9ebce56c313b58f990b07e
evgeny <ivanov.evgen.v@gmail.com>
2021-07-13 20:50:32 +0500

    Point test

--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
 import ru.job4j.triangle.Triangle;
 
@@ -9,14 +10,34 @@ import static org.junit.Assert.assertThat;
 
 public class PointTest {
     @Test
-    public void whenZeroAndTenThenTen() {
-        // Создаем объект треугольник
-        Triangle triangle = new Triangle();
-        // Вычисляем площадь треугольника
-        double result = triangle.area(0, 0, 0, 2, 2, 0);
-        // Задаем ожидаемый  результат
+    public void when00to20then2() {
+        Point point = new Point();
+        double result = point.distance(0, 0, 2, 0);
         double expected = 2D;
-        // Проверяем результат и ожидаемое значение
-        assertThat(result, closeTo(expected, 0.1));
+        Assert.assertEquals(result, expected, 0.1);
+    }
+
+    @Test
+    public void when12to21then14() {
+        Point point = new Point();
+        double result = point.distance(1, 2, 2, 1);
+        double expected = 1.414D;
+        Assert.assertEquals(result, expected, 0.1);
+    }
+
+    @Test
+    public void when35to149then11705() {
+        Point point = new Point();
+        double result = point.distance(3, 5, 14, 9);
+        double expected = 11.705D;
+        Assert.assertEquals(result, expected, 0.1);
+    }
+
+    @Test
+    public void when819to1344then25495() {
+        Point point = new Point();
+        double result = point.distance(8, 19, 13, 44);
+        double expected = 25.495D;
+        Assert.assertEquals(result, expected, 0.1);
     }
 }
