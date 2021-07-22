6dcb4043e8d6dac3ec258b52ce0b9c19ebc863f5
evgeny <ivanov.evgen.v@gmail.com>
2021-07-22 14:03:37 +0500

    Entrance to the cinema

--- chapter_001/src/main/java/ru/job4j/triangle/SqArea.java
+++ chapter_001/src/main/java/ru/job4j/triangle/SqArea.java
@@ -12,5 +12,4 @@ public class SqArea {
         System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
         System.out.println(" p = 6, k = 2, s = 2, real = " + result2);
     }
-
 }
--- chapter_001/src/test/java/ru/job4j/triangle/SqAreaTest.java
+++ chapter_001/src/test/java/ru/job4j/triangle/SqAreaTest.java
@@ -39,6 +39,4 @@ public class SqAreaTest  {
         double out = SqArea.square(p, k);
         Assert.assertEquals(expected, out, 0.01);
     }
-
-
 }
