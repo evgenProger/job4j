c6be3b39e31bb1898f0abd698fafe85e1683ed64
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-10-10 17:10:26 +0500

    Refactor ChessBoard

--- chapter_001/src/main/java/ru/job4j/condition/ChessBoard.java
+++ chapter_001/src/main/java/ru/job4j/condition/ChessBoard.java
@@ -9,10 +9,6 @@ public class ChessBoard {
     }
 
     public static boolean isDiagonal(int x1, int y1, int x2, int y2) {
-        boolean rs = false;
-        if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
-            rs = true;
-        }
-        return rs;
+        return Math.abs(x2 - x1) == Math.abs(y2 - y1);
     }
 }
