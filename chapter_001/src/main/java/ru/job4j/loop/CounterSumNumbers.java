405ddabd8083d332e2563ce93937f561e9c0d296
evgeny <ivanov.evgen.v@gmail.com>
2021-10-15 14:22:00 +0500

    Counter Sum Numbers

---
+++ chapter_001/src/main/java/ru/job4j/loop/CounterSumNumbers.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class CounterSumNumbers {
+    public class Counter {
+        public static int sum(int start, int finish) {
+            int sum = 0;
+            for (int i = start; i < finish; i++) {
+                sum = sum + i;
+            }
+            return sum;
+        }
+    }
+}
