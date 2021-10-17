c6efb8755ecb8949d28eec203c4404fbe2113b46
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-10-17 17:23:38 +0500

    Counter Mortgage

--- chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,16 +4,12 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         percent = percent / 100;
-        amount  += amount * percent;
-        while (amount > 0) {
-            year++;
-            amount  -= salary;
-            if (amount < 0) {
-                break;
-            }
-            amount = amount * percent + amount;
 
-       }
+        while (amount > 0) {
+            amount  += amount * percent;
+            amount  -= salary;
+            year++;
+        }
         return year;
     }
 }
