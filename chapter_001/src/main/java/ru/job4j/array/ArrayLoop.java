4a86c17756f56592c3e521a6fb484a2db49ed0ae
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-10-19 21:53:09 +0500

    Use loop for-each for array

--- chapter_001/src/main/java/ru/job4j/array/ArrayLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayLoop.java
@@ -9,5 +9,8 @@ public class ArrayLoop {
         for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
         }
+        for (int number : numbers) {
+            System.out.println(number);
+        }
     }
 }
