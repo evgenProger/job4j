c2316f82fc2e7ed081917741fef3bdf5cfbad339
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-10-19 16:41:08 +0500

    Output elements array

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,8 +5,16 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Petr";
+        names[1] = "Evgeny";
+        names[2] = "Masha";
+        names[3] = "Dasha";
         System.out.println("ages size = " + ages.length);
         System.out.println("surnames size = " + surnames.length);
         System.out.println("prices size = " + prices.length);
+        for (int i = 0; i < 4; i++) {
+            System.out.println("name " + names[i]);
+        }
     }
 }
