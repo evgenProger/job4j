c8fb4574af8e720439050b5fa7388b840054455a
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-10-24 21:59:24 +0500

    Finding the index of an element into array

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.array;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -26,6 +27,40 @@ public class FindLoopTest {
         int expect = -1;
         int result = notfind.indexOf(input, value);
         assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHasLength5Then0() {
+        FindLoop findLoop = new FindLoop();
+        int[] data = new int[] {5, 10, 3};
+        int el = 5;
+        int result = findLoop.indexOf(data, el);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenFind3() {
+        FindLoop findLoop = new FindLoop();
+        int[] data = new int[] {5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = 3;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenNotFind() {
+        FindLoop findLoop = new FindLoop();
+        int[] data = new int[] {12, 24, 7, 32, 78};
+        int el = 6;
+        int start = 1;
+        int finish = 4;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
     }
 
 
