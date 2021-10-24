6b62b8f66fa0a1dfcab2246972c608a453e154bb
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-10-24 21:12:04 +0500

    Word end with...

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -3,17 +3,12 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        int j = 0;
         for (int i = 0; i < post.length; i++) {
-            if (post[post.length - i - 1] != word[word.length - j - 1]) {
+            if (post[post.length - i - 1] != word[word.length - i - 1]) {
                 result = false;
                 break;
             }
-            j++;
         }
-
-
         return result;
     }
-
 }
