c78d872c6c42d41542c45de19398eec212a44056
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-11-11 12:52:59 +0500

    Mono vertical checking in the array

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -13,7 +13,7 @@ public class MatrixCheck {
     }
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
-        for (int i = 0; i < board.length; i++ ) {
+        for (int i = 0; i < board.length; i++) {
             if (board[i][column] != 'X') {
                 result = false;
                 break;
