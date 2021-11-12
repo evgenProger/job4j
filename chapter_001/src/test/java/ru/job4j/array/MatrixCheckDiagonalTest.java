4c67693707e1b24644eb48492ab43f8a23c47d52
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-11-12 23:49:01 +0500

    Check mono line into matrix

---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckDiagonalTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class MatrixCheckDiagonalTest {
+    @Test
+    public void whenDateMonoByTrueThenTrue() {
+        MatrixCheckDiagonal check = new MatrixCheckDiagonal();
+        boolean[][] input = new boolean[][]{
+                {true, true, true},
+                {false, true, true},
+                {true, false, true}
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+
+        @Test
+        public void whenDateNotMonoByTrueThenFalse() {
+            MatrixCheckDiagonal check = new MatrixCheckDiagonal();
+            boolean[][] input = new boolean[][]{
+                    {true, true, false},
+                    {false, false, true},
+                    {true, false, true}
+            };
+            boolean result = check.mono(input);
+            assertThat(result, is(false));
+        }
+
+
+    }
+
