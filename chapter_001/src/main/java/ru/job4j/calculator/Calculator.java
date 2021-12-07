2d0250310d7ecaad510a607bc0c3d6d818ab025e
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-12-07 20:52:41 +0500

    Overload method Add.

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -3,8 +3,20 @@ package ru.job4j.calculator;
 public class Calculator {
 
     public double add(double first, double second) {
-
         return first + second;
+    }
+
+    public double add(double first, double second, double third) {
+        return add(
+                first,
+                add(second, third)
+        );
+    }
+
+    public double add(double first, double second, double third, double four) {
+        return add(first,
+                add(second,
+                        add(third, four)));
     }
 
     public double subtract(double first, double second) {
