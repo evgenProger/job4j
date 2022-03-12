3f619dcc54478de0d31e332fb9c0e0b5ff27222d
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2022-03-12 18:13:57 +0500

    Delete model from array

--- chapter_002/src/main/java/ru/jobj4/pojo/ShopDrop.java
+++ chapter_002/src/main/java/ru/jobj4/pojo/ShopDrop.java
@@ -2,8 +2,9 @@ package ru.jobj4.pojo;
 
 public class ShopDrop {
     public static Product[] delete(Product[] products, int index) {
-        int len = (products.length - 1) - index;
-        System.arraycopy(products, index + 1, products, index, len);
+        for (int i = index + 1; i < products.length; i++) {
+            products[i - 1] = products[i];
+        }
         products[products.length - 1] = null;
         return products;
     }
