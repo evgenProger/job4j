964a1b83f05eb12385759a0b00335d36be0b6db1
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2022-05-30 17:20:30 +0500

    Demo extends

---
+++ chapter_002/src/main/java/ru/job4j/inheritance/PizzaExtraCheese.java
@@ -0,0 +1,9 @@
+package ru.job4j.inheritance;
+
+public class PizzaExtraCheese extends Pizza {
+
+    @Override
+    public String name() {
+        return "Pizza with extra cheese";
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/inheritance/PizzaExtraCheeseExtraTomato.java
@@ -0,0 +1,9 @@
+package ru.job4j.inheritance;
+
+public class PizzaExtraCheeseExtraTomato extends Pizza {
+
+    @Override
+    public String name() {
+        return "Pizza with extra cheese and tomato";
+    }
+}
