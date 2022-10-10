c751082c9ca8f527a2e8b4bbc4f135acd7f68e42
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2022-10-10 21:50:42 +0500

    exception hierarchy and multiple catch  [#219368]

---
+++ chapter_002/src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,40 @@
+package ru.job4j.ex;
+
+public class UserStore {
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User result = null;
+        for (User u : users) {
+            if (u.getUsername().equals(login)) {
+                result = u;
+                break;
+            }
+        }
+        if (result == null) {
+            throw new UserNotFoundException("User not found");
+        }
+        return result;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid() || user.getUsername().length() < 3) {
+            throw new UserInvalidException("User is not valid");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserNotFoundException en) {
+            en.printStackTrace();
+        } catch (UserInvalidException ep) {
+            ep.printStackTrace();
+        }
+    }
+}
