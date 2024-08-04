6dbad42215f7704ede5dd7d0039bdd20c095a7b1
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2024-08-04 18:35:56 +0500

    1. Интерфейс Queue [#504942]

---
+++ chapter_003/src/main/java/ru/job4j/collection/queue/Customer.java
@@ -0,0 +1,12 @@
+package ru.job4j.collection.queue;
+
+public class Customer {
+    String name;
+    int amount;
+
+
+    public Customer(String name, int amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+}
